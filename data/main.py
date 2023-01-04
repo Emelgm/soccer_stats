a8915612da82578d04c47d3c87b7025c1cb33c7c
Emel GM <emel.siete@gmail.com>
2023-01-03 22:28:35 -0500

    escoger ligas opcionales

--- data/main.py
+++ data/main.py
@@ -15,23 +15,35 @@ content = web_page.text
 # ordenar estructura html
 web_body = BeautifulSoup(web_page.text, 'lxml')
 # extraer partidos de ligas
-ligues_matches = web_body.find_all('div', attrs={
+leagues_matches = web_body.find_all('div', attrs={
     'class':'table_wrapper tabbed'
     })
 # links de cada liga
-links_ligues = [url + link.a.get('href') for link in ligues_matches]
-names_ligues = [link.a.text for link in ligues_matches]
+links_leagues = [url + link.a.get('href') for link in leagues_matches]
+names_leagues = [link.a.text for link in leagues_matches]
 
 # enpaquetar en diccionario
 dict_links = {}
-for n,l in zip(names_ligues,links_ligues):
+for n,l in zip(names_leagues,links_leagues):
     dict_links[n] = l
 
 #print(dict_links)
 # listar opciones
-print('Options list:\n')
+name_list = []
+number_list = []
+print('\nOptions list:\n')
 for idx,i in enumerate(dict_links.items()):
     print(f'[{idx + 1}] - {i[0]}')
+    name_list.append(i[0])
+    number_list.append(idx + 1)
 
-#option = input('Escoge una liga: ')
-#if option == 'A'
+# elegir opcion de liga
+option = int(input('\nChoose a league: '))
+try:
+    if option == number_list[option - 1]:
+        link_league = dict_links[name_list[option - 1]]
+        print(f'Name League: {name_list[option - 1]}')
+        league_page = requests.get(link_league)
+        print(f'Respuesta HTTP: {league_page.status_code}')
+except IndexError as e:
+    print('\nindex out of range\ntry again')

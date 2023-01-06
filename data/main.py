d005361a507e493173fbb0a37765257d78bdaaae
Emel GM <emel.siete@gmail.com>
2023-01-06 15:22:11 -0500

    segmentando por funciones

--- data/main.py
+++ data/main.py
@@ -1,133 +1,144 @@
 from bs4 import BeautifulSoup
-import numpy as pd
 import requests
 import pandas as pd
 
-URL = 'https://fbref.com/es/partidos/'
-url = 'https://fbref.com'
-LIGUES = ['Premier League', 'La Liga', 'Ligue 1', 'Bundesliga', 'Serie A']
-
-web_page = requests.get(URL)
-print(f'Respuesta HTTP: {web_page.status_code}')
-
-# contenido html
-content = web_page.text
 # ordenar estructura html
-web_body = BeautifulSoup(web_page.text, 'lxml')
-# extraer partidos de ligas
-leagues_matches = web_body.find_all('div', attrs={
-    'class':'table_wrapper tabbed'
+def leagues_list(url, url_web):
+    web_body = BeautifulSoup(url_web.text, 'lxml')
+    # extraer partidos de ligas
+    leagues_matches = web_body.find_all('div', attrs={
+        'class':'table_wrapper tabbed'
+        })
+    # links de cada liga
+    links_leagues = [url + link.a.get('href') for link in leagues_matches]
+    names_leagues = [link.a.text for link in leagues_matches]
+
+    # enpaquetar en diccionario
+    dict_links = {}
+    for n,l in zip(names_leagues,links_leagues):
+        dict_links[n] = l
+
+    return dict_links
+
+
+def links_list(links):
+    # listar opciones
+    name_list = []
+    number_list = []
+    print('\nOptions list:\n')
+    for idx,i in enumerate(links.items()):
+        print(f'[{idx + 1}] - {i[0]}')
+        name_list.append(i[0])
+        number_list.append(idx + 1)
+
+    # elegir opcion de liga
+    option = int(input('\nChoose a league: '))
+    try:
+        if option == number_list[option - 1]:
+            link_league = links[name_list[option - 1]]
+            print(f'Name League: {name_list[option - 1]}')
+            league_page = requests.get(link_league)
+            print(f'Respuesta HTTP: {league_page.status_code}')
+    except IndexError as e:
+        print('\nindex out of range\ntry again')
+    
+    return league_page
+
+def detail_matches(url, url_web):
+    # detalle de partidos
+    web_league = BeautifulSoup(url_web.text, 'lxml')
+    scores = web_league.find('div', attrs={
+        'class': 'inactive'
+    }).find_all('li', attrs={
+        'class': 'full'
     })
-# links de cada liga
-links_leagues = [url + link.a.get('href') for link in leagues_matches]
-names_leagues = [link.a.text for link in leagues_matches]
+    # detalle de marcadores
+    link_score = url + scores[1].a.get('href')
+    link_matches = requests.get(link_score)
+    web_scores = BeautifulSoup(link_matches.text, 'lxml')
+    matches = web_scores.find('tbody').find_all('td')
+    home_team = [link.get_text() for link in matches]
+    output=[home_team[i:i + 13] for i in range(0, len(home_team), 13)]
 
-# enpaquetar en diccionario
-dict_links = {}
-for n,l in zip(names_leagues,links_leagues):
-    dict_links[n] = l
+    return output
 
-#print(dict_links)
-# listar opciones
-name_list = []
-number_list = []
-print('\nOptions list:\n')
-for idx,i in enumerate(dict_links.items()):
-    print(f'[{idx + 1}] - {i[0]}')
-    name_list.append(i[0])
-    number_list.append(idx + 1)
+def df_create(df_dict):
+    out_dict = {
+        'dia': '',
+        'fecha': '',
+        'hora': '',
+        'equipo_local': '',
+        'gx': '',
+        'marcador': '',
+        'gy': '',
+        'equipo_visitante': '',
+        'p': '',
+        'estadio': '',
+        'arbitro': '',
+        'informe': '',
+        'x': ''
+    }
 
-# elegir opcion de liga
-option = int(input('\nChoose a league: '))
-try:
-    if option == number_list[option - 1]:
-        link_league = dict_links[name_list[option - 1]]
-        print(f'Name League: {name_list[option - 1]}')
-        league_page = requests.get(link_league)
-        print(f'Respuesta HTTP: {league_page.status_code}')
-except IndexError as e:
-    print('\nindex out of range\ntry again')
+    dia_list = []
+    fecha_list = []
+    hora_list = []
+    el_list = []
+    gx_list = []
+    marcador_list = []
+    gy_list = []
+    ev_list = []
+    p_list = []
+    estadio_list = []
+    arbitro_list = []
+    inf_list = []
+    x_list = []
+    for i in df_dict:
+        dia_list.append(i[0])
+        out_dict['dia'] = dia_list
+        fecha_list.append(i[1])
+        out_dict['fecha'] = fecha_list
+        hora_list.append(i[2])
+        out_dict['hora'] = hora_list
+        el_list.append(i[3])
+        out_dict['equipo_local'] = el_list
+        gx_list.append(i[4])
+        out_dict['gx'] = gx_list
+        marcador_list.append(i[5])
+        out_dict['marcador'] = marcador_list
+        gy_list.append(i[6])
+        out_dict['gy'] = gy_list
+        ev_list.append(i[7])
+        out_dict['equipo_visitante'] = ev_list
+        p_list.append(i[8])
+        out_dict['p'] = p_list
+        estadio_list.append(i[9])
+        out_dict['estadio'] = estadio_list
+        arbitro_list.append(i[10])
+        out_dict['arbitro'] = arbitro_list
+        inf_list.append(i[11])
+        out_dict['informe'] = inf_list
+        x_list.append(i[12])
+        out_dict['x'] = x_list
 
-# detalle de partidos
-web_league = BeautifulSoup(league_page.text, 'lxml')
-scores = web_league.find('div', attrs={
-    'class': 'inactive'
-}).find_all('li', attrs={
-    'class': 'full'
-})
-# detalle de marcadores
-link_score = url + scores[1].a.get('href')
-link_matches = requests.get(link_score)
-web_scores = BeautifulSoup(link_matches.text, 'lxml')
-matches = web_scores.find('tbody').find_all('td')
-home_team = [link.get_text() for link in matches]
-output=[home_team[i:i + 13] for i in range(0, len(home_team), 13)]
-out_dict = {
-    'dia': '',
-    'fecha': '',
-    'hora': '',
-    'equipo_local': '',
-    'gx': '',
-    'marcador': '',
-    'gy': '',
-    'equipo_visitante': '',
-    'p': '',
-    'estadio': '',
-    'arbitro': '',
-    'informe': '',
-    'x': ''
-}
+    df = pd.DataFrame(out_dict)
+    #print(df.head())
 
-# names_list = ['dia', 'fecha', 'hora', 'equipo_local', 'gx',
-#     'marcador','gy', 'equipo_visitante', 'p', 'estadio', 'arbritro',
-#     'informe'
-# ]
+    # guardar en archivo
+    df.to_csv('./data/raw/matches.csv')
 
-dia_list = []
-fecha_list = []
-hora_list = []
-el_list = []
-gx_list = []
-marcador_list = []
-gy_list = []
-ev_list = []
-p_list = []
-estadio_list = []
-arbitro_list = []
-inf_list = []
-x_list = []
-for i in output:
-    dia_list.append(i[0])
-    out_dict['dia'] = dia_list
-    fecha_list.append(i[1])
-    out_dict['fecha'] = fecha_list
-    hora_list.append(i[2])
-    out_dict['hora'] = hora_list
-    el_list.append(i[3])
-    out_dict['equipo_local'] = el_list
-    gx_list.append(i[4])
-    out_dict['gx'] = gx_list
-    marcador_list.append(i[5])
-    out_dict['marcador'] = marcador_list
-    gy_list.append(i[6])
-    out_dict['gy'] = gy_list
-    ev_list.append(i[7])
-    out_dict['equipo_visitante'] = ev_list
-    p_list.append(i[8])
-    out_dict['p'] = p_list
-    estadio_list.append(i[9])
-    out_dict['estadio'] = estadio_list
-    arbitro_list.append(i[10])
-    out_dict['arbitro'] = arbitro_list
-    inf_list.append(i[11])
-    out_dict['informe'] = inf_list
-    x_list.append(i[12])
-    out_dict['x'] = x_list
-#print(out_dict)
+    return df
 
-df = pd.DataFrame(out_dict)
-print(df.head())
+def run():
+    URL = 'https://fbref.com/es/partidos/'
+    url = 'https://fbref.com'
 
-# guardar en archivo
-df.to_csv('./data/raw/matches.csv')
+    link_page = requests.get(URL)
+    print(f'Respuesta HTTP: {link_page.status_code}')
+    dict_list = leagues_list(url, link_page)
+    league_pague = links_list(dict_list)
+    output = detail_matches(url, league_pague)
+    df_create(output)
 
+
+if __name__=='__main__':
+    run()
